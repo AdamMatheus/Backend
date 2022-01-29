31115a6601d35f3960b055b6ff4f6f4bfda1cf98
AdamMatheus <adam.matheus.it@gmail.com>
2022-01-29 14:52:17 +0100

    ok

--- Dj_7_Project/todo/views.py
+++ Dj_7_Project/todo/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "todo/home.html")
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+
+    context = {"todos": todos}
+
+    return render(request, "todo/todo_list.html", context)
+
+
+def todo_add(request):
+
+    form = TodoForm()
+
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {"form": form}
+
+    return render(request, "todo/todo_add.html", context)
+
+
+def todo_update(request, id):
+    pass
+
+
+def todo_delete(request, id):
+    pass
