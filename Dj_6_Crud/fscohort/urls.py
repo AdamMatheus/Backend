9382f6a075358fd245e3ddc50ce12ae092b85931
AdamMatheus <adam.matheus.it@gmail.com>
2022-01-29 10:49:52 +0100

    ok

--- Dj_6_Crud/fscohort/urls.py
+++ Dj_6_Crud/fscohort/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, student_list, student_add
 
 urlpatterns = [
     path('', home_page, name="home"),
+    path('student_list/', student_list, name="list"),
+    path('student_add/', student_add, name="add"),
 
 ]
