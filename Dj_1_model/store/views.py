f78279d030a4264108cb49e752ab4438d41e852a
AdamMatheus <adam.matheus.it@gmail.com>
2022-02-01 18:17:05 +0100

    ok

--- Dj_1_model/store/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http.response import HttpResponse
-
-# Create your views here.
-
-def  index(request):
-    return HttpResponse("Clarusway welcome")
