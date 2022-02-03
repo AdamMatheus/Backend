78c1c04d1502d5ac2bf287488b5e46789c76a6a2
AdamMatheus <adam.matheus.it@gmail.com>
2022-02-03 20:33:15 +0100

    ok

--- Dj-11_Auth/user_example/urls.py
+++ Dj-11_Auth/user_example/urls.py
@@ -1,11 +1,8 @@
 
 from django.urls import path
-from .views import home_view, special, register
-from django.contrib.auth import views as auth_views
+from .views import home,register
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('special/', special, name='special'),
-    path('register/', register, name='register'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change")
+    path('', home, name='home'),
+    path('register/',register,name='register'),
 ]
