55f2bc75a0559107d7d3b088ff7ecaf207f6c3f8
AdamMatheus <adam.matheus.it@gmail.com>
2022-02-17 18:03:59 +0100

    ok

--- Dj_recap/django_1_recap/home/views.py
+++ Dj_recap/django_1_recap/home/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return render(request,"home/index.html")
+    return render(request, "home/index.html")
+
+
+def about(request):
+    return render(request, "home/about.html")
+
+
+def teacher(request):
+    return render(request, "home/teacher.html")
