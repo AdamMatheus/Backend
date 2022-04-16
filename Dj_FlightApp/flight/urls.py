349c04e645e56672c310de78fc715cf15707e940
AdamMatheus <adam.matheus.it@gmail.com>
2022-04-16 21:06:18 +0200

    added file

--- Dj_FlightApp/flight/urls.py
+++ Dj_FlightApp/flight/urls.py
@@ -1,43 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from django.urls import path
+from rest_framework import routers
+from .views import FlightView, ReservationView
 
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Flight Reservation API",
-        default_version="v1",
-        description="Flight Reservation API",
-        terms_of_service="#",
-        contact=openapi.Contact(email="aaron@clarusway.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
+router = routers.DefaultRouter()
+router.register('flights', FlightView)
+router.register('resv', ReservationView)
 
 urlpatterns = [
 
-    # setup
-    path("admin/", admin.site.urls),
-    path(
-        "swagger(<format>\.json|\.yaml)",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc",
-         cache_timeout=0), name="schema-redoc"),
-    path("__debug__/", include("debug_toolbar.urls")),
-
-    # apps urls
-    path("users/", include("users.urls")),
-    path("flight/", include("flight.urls"))
 ]
+
+urlpatterns += router.urls
--- Dj_FlightApp/main/urls.py
+++ Dj_FlightApp/main/urls.py
@@ -20,8 +20,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    
-    #setup
+
+    # setup
     path("admin/", admin.site.urls),
     path(
         "swagger(<format>\.json|\.yaml)",
@@ -33,9 +33,11 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("redoc/", schema_view.with_ui("redoc",
+         cache_timeout=0), name="schema-redoc"),
     path("__debug__/", include("debug_toolbar.urls")),
-    
-    #apps urls
+
+    # apps urls
     path("users/", include("users.urls")),
+    path("flight/", include("flight.urls")),
 ]
